6c27f415f4202bdd2d3f206aad03eaec95f09a59
The M. Do <dominhthe110@gmail.com>
2017-08-30 19:00:13 +0700

    Wed Aug 30 7:000PM

--- 4_46_Page_The_Scroll/ViewController.swift
+++ 4_46_Page_The_Scroll/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [UIImageView]()
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            images.append(UIImageView(image: image))
+        }
+        
+        print("Count: \(images.count)")
     }
 
     override func didReceiveMemoryWarning() {
