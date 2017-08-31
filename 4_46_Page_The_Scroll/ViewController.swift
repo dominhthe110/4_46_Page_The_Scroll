4613ae71197b3259b40b99e93e1e22bcbd1e3739
The M. Do <dominhthe110@gmail.com>
2017-08-31 15:12:51 +0700

    Thu Aug 31 3:12PM
    Undone

--- 4_46_Page_The_Scroll/ViewController.swift
+++ 4_46_Page_The_Scroll/ViewController.swift
@@ -12,18 +12,39 @@ class ViewController: UIViewController {
     //Hello
     var images = [UIImageView]()
     
+    
     @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var contentWidth : CGFloat = 0.0
+        
+        print("Scrollview width: \(scrollView.frame.size.width)")
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
         }
         
-        print("Count: \(images.count)")
+        //scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.backgroundColor = UIColor.lightGray
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +52,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
 
 }
 
